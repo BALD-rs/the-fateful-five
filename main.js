0998c47b721477bd571e26fde60e9f576d4df474
Anton Angeletti <angelettianton@gmail.com>
2024-11-09 19:56:44 -0600

    background exists

--- main.js
+++ main.js
@@ -1,20 +1,35 @@
 var config = {
     type: Phaser.AUTO,
-    width: 960,
-    height: 540,
+    width: 1920,
+    height: 900,
     scene: {
         preload: preload,
         create: create,
         update: update
-    }
+    },
+    antialias: false,
 };
 
 var game = new Phaser.Game(config);
 
 function preload() {
+    this.load.spritesheet('backgroundSprite', 'assets/sprites/background.png', {
+        frameWidth: 200,
+        frameHeight: 100,
+    });
 }
 
 function create() {
+    this.anims.create({
+        key: 'background',
+        frames: this.anims.generateFrameNumbers('backgroundSprite', {
+            start: 0,
+            end: 6
+        }),
+        frameRate: 10,
+        repeat: -1,
+    })
+    const background = this.add.sprite(config.width/2, config.height/2, 'backgroundSprite').setScale(8).play('background');
 }
 
 function update() {
